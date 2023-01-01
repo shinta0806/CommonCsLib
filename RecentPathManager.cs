73d6c35451b69e4f5483c4c7e2d03f76fa48cf15
shinta0806 <shinta.0806@gmail.com>
2023-01-01 10:10:42 +0900

    ニコカラメーカー 2 Ver 12.60 時点

---
+++ RecentPathManager.cs
@@ -0,0 +1,189 @@
+﻿// ============================================================================
+// 
+// 最近使用したファイル・フォルダーを管理
+// Copyright (C) 2022 by SHINTA
+// 
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// 
+// ----------------------------------------------------------------------------
+
+// ============================================================================
+//  Ver.  |      更新日      |                    更新内容
+// ----------------------------------------------------------------------------
+//  -.--  | 2022/12/31 (Sat) | 作成開始。
+//  1.00  | 2022/12/31 (Sat) | ファーストバージョン。
+// ============================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace Shinta;
+
+// ====================================================================
+// public 列挙子
+// ====================================================================
+
+/// <summary>
+/// 履歴種別（ファイル／フォルダー）
+/// </summary>
+[Flags]
+public enum RecentItemType
+{
+	File = 0x01,
+	Folder = 0x02,
+	FileAndFolder = File | Folder,
+}
+
+
+internal class RecentPathManager
+{
+	// ====================================================================
+	// コンストラクター
+	// ====================================================================
+
+	/// <summary>
+	/// メインコンストラクター
+	/// </summary>
+	/// <param name="recentItemType">履歴種別</param>
+	/// <param name="capacity">保持する履歴の数</param>
+	public RecentPathManager(RecentItemType recentItemType, Int32 capacity)
+	{
+		_recentItemType = recentItemType;
+		_capacity = Math.Max(capacity, 1);
+	}
+
+	// ====================================================================
+	// public 関数
+	// ====================================================================
+
+	/// <summary>
+	/// 履歴を追加
+	/// </summary>
+	/// <param name="path">履歴のパス</param>
+	/// <returns>追加したら true</returns>
+	public Boolean Add(String path)
+	{
+		ConfirmRecentExist();
+
+		if (!Exists(path))
+		{
+			return false;
+		}
+
+		Int32 existIndex = FindIndex(path);
+		if (existIndex >= 0)
+		{
+			// 既に追加されている場合はいったん削除
+			_recentPathes.RemoveAt(existIndex);
+		}
+
+		// 先頭に追加
+		_recentPathes.Insert(0, path);
+
+		// 溢れた履歴を削除
+		Truncate();
+
+		return true;
+	}
+
+	/// <summary>
+	/// 履歴
+	/// </summary>
+	/// <returns></returns>
+	public ReadOnlyCollection<String> RecentPathes()
+	{
+		return _recentPathes.AsReadOnly();
+	}
+
+	/// <summary>
+	/// 履歴を指定されたパス群で置き換える
+	/// </summary>
+	/// <param name="pathes"></param>
+	public void SetPathes(IEnumerable<String> pathes)
+	{
+		_recentPathes = new(pathes);
+		ConfirmRecentExist();
+		Truncate();
+	}
+
+	// ====================================================================
+	// private 変数
+	// ====================================================================
+
+	/// <summary>
+	/// 履歴種別
+	/// </summary>
+	private RecentItemType _recentItemType;
+
+	/// <summary>
+	/// 保持する履歴の数
+	/// </summary>
+	private Int32 _capacity;
+
+	/// <summary>
+	/// 履歴
+	/// 先頭が最新
+	/// </summary>
+	private List<String> _recentPathes = new();
+
+	// ====================================================================
+	// private 関数
+	// ====================================================================
+
+	/// <summary>
+	/// 履歴のファイル・フォルダーが現存しているか確認し、現存していない場合は履歴から削除
+	/// </summary>
+	private void ConfirmRecentExist()
+	{
+		for (Int32 i = _recentPathes.Count - 1; i >= 0; i--)
+		{
+			if (!Exists(_recentPathes[i]))
+			{
+				_recentPathes.RemoveAt(i);
+			}
+		}
+	}
+
+	/// <summary>
+	/// 履歴種別に合致したものが存在しているか
+	/// </summary>
+	/// <param name="path"></param>
+	/// <returns></returns>
+	private Boolean Exists(String path)
+	{
+		if (_recentItemType.HasFlag(RecentItemType.File) && File.Exists(path))
+		{
+			return true;
+		}
+		if (_recentItemType.HasFlag(RecentItemType.Folder) && Directory.Exists(path))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	/// <summary>
+	/// 指定されたパスが履歴の何番目にあるか
+	/// </summary>
+	/// <param name="path"></param>
+	/// <returns></returns>
+	private Int32 FindIndex(String path)
+	{
+		return _recentPathes.FindIndex(x => String.Compare(x, path, true) == 0);
+	}
+
+	/// <summary>
+	/// 溢れた履歴を削除
+	/// </summary>
+	private void Truncate()
+	{
+		if (_recentPathes.Count > _capacity)
+		{
+			_recentPathes.RemoveRange(_capacity, _recentPathes.Count - _capacity);
+		}
+	}
+}
