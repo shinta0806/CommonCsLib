ebaeb294021fb8e0fb9b890eab7a5311afd2c48e
shinta0806 <shinta.0806@gmail.com>
2023-03-25 13:54:38 +0900

    ニコカラメーカー 3 Ver 0.99.029 時点

--- WinUi3/Views/PageEx2.cs
+++ WinUi3/Views/PageEx2.cs
@@ -14,12 +14,17 @@
 //  -.--  | 2022/12/08 (Thu) | 作成開始。
 //  1.00  | 2022/12/08 (Thu) | ファーストバージョン。
 // (1.01) | 2023/01/04 (Wed) |   PageEx2Loaded() で表示スケールを考慮するようにした。
+//  1.10  | 2023/03/25 (Sat) | IsCustomTitleBarEnabled を作成。
 // ============================================================================
 
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
 
 using Serilog;
+
+using System.Diagnostics;
 
 using Windows.Graphics;
 
@@ -44,6 +49,22 @@ public class PageEx2 : Page
 	}
 
 	// ====================================================================
+	// public プロパティー
+	// ====================================================================
+
+	/// <summary>
+	/// ELEMENT_NAME_CUSTOM_TITLE_BAR をカスタムタイトルバーとして使用する
+	/// </summary>
+	public readonly DependencyProperty IsCustomTitleBarEnabledProperty
+			= DependencyProperty.Register(nameof(IsCustomTitleBarEnabled), typeof(Boolean), typeof(PageEx2),
+			new PropertyMetadata(false, new PropertyChangedCallback(OnIsCustomTitleBarEnabledChanged)));
+	public Boolean IsCustomTitleBarEnabled
+	{
+		get => (Boolean)GetValue(IsCustomTitleBarEnabledProperty);
+		set => SetValue(IsCustomTitleBarEnabledProperty, value);
+	}
+
+	// ====================================================================
 	// protected 変数
 	// ====================================================================
 
@@ -57,6 +78,28 @@ public class PageEx2 : Page
 	// ====================================================================
 
 	/// <summary>
+	/// イベントハンドラー
+	/// </summary>
+	/// <param name="dependencyObject"></param>
+	/// <param name="args"></param>
+	private static void OnIsCustomTitleBarEnabledChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+	{
+		if (dependencyObject is not PageEx2 pageEx2)
+		{
+			return;
+		}
+		if (!AppWindowTitleBar.IsCustomizationSupported())
+		{
+			return;
+		}
+
+		pageEx2._window.AppWindow.TitleBar.ExtendsContentIntoTitleBar = pageEx2.IsCustomTitleBarEnabled;
+
+		// ToDo: Windows App SDK 1.2 現在、効果が無い模様
+		pageEx2._window.AppWindow.TitleBar.IconShowOptions = IconShowOptions.ShowIconAndSystemMenu;
+	}
+
+	/// <summary>
 	/// イベントハンドラー：ページがロードされた
 	/// </summary>
 	/// <param name="sender"></param>
@@ -65,35 +108,8 @@ public class PageEx2 : Page
 	{
 		try
 		{
-			// WindowEx2.SizeToContent の処理
-			// ToDo: Window.SizeToContent が実装されれば不要となるコード
-			if (_window.SizeToContent != SizeToContent.Manual)
-			{
-				// Windows App SDK 1.2 現在、コンテンツのサイズは表示スケールに自動追随するが、ResizeClient() は表示スケールを考慮してくれない
-				Double scale = WinUi3Common.DisplayScale(_window);
-
-				Int32 width = _window.AppWindow.ClientSize.Width;
-				if (_window.SizeToContent == SizeToContent.Width || _window.SizeToContent == SizeToContent.WidthAndHeight)
-				{
-					width = (Int32)Content.ActualSize.X;
-					if (Content is FrameworkElement frameworkElement)
-					{
-						width += (Int32)(frameworkElement.Margin.Left + frameworkElement.Margin.Right);
-					}
-					width = (Int32)(width * scale);
-				}
-				Int32 height = _window.AppWindow.ClientSize.Height;
-				if (_window.SizeToContent == SizeToContent.Height || _window.SizeToContent == SizeToContent.WidthAndHeight)
-				{
-					height = (Int32)Content.ActualSize.Y;
-					if (Content is FrameworkElement frameworkElement)
-					{
-						height += (Int32)(frameworkElement.Margin.Top + frameworkElement.Margin.Bottom);
-					}
-					height = (Int32)(height * scale);
-				}
-				_window.AppWindow.ResizeClient(new SizeInt32(width, height));
-			}
+			WindowSizeToContent();
+			SetCustomTitleBar();
 		}
 		catch (Exception ex)
 		{
@@ -101,5 +117,68 @@ public class PageEx2 : Page
 			Log.Error("ページロード時エラー：" + GetType().Name + "\n" + ex.Message);
 			Log.Information("スタックトレース：\n" + ex.StackTrace);
 		}
+	}
+
+	/// <summary>
+	/// ELEMENT_NAME_CUSTOM_TITLE_BAR をカスタムタイトルバーとして設定
+	/// </summary>
+	private void SetCustomTitleBar()
+	{
+		if (!IsCustomTitleBarEnabled || !AppWindowTitleBar.IsCustomizationSupported())
+		{
+			return;
+		}
+		FrameworkElement? customTitleBar = FindName(WinUi3Common.ELEMENT_NAME_CUSTOM_TITLE_BAR) as FrameworkElement;
+		if (customTitleBar == null)
+		{
+			return;
+		}
+
+		// ドラッグできるようにする
+		RectInt32[] rects = { new RectInt32((Int32)customTitleBar.ActualOffset.X, 0, Int32.MaxValue, (Int32)customTitleBar.ActualHeight) };
+		_window.AppWindow.TitleBar.SetDragRectangles(rects);
+
+		// ボタンの色をカスタムタイトルバーに合わせる
+		if (customTitleBar is Panel panel && panel.Background is SolidColorBrush brush)
+		{
+			_window.AppWindow.TitleBar.ButtonBackgroundColor = brush.Color;
+		}
+	}
+
+	/// <summary>
+	/// WindowEx2.SizeToContent の処理
+	/// </summary>
+	private void WindowSizeToContent()
+	{
+		if (_window.SizeToContent == SizeToContent.Manual)
+		{
+			return;
+		}
+
+		// ToDo: Window.SizeToContent が実装されれば不要となるコード
+		// Windows App SDK 1.2 現在、コンテンツのサイズは表示スケールに自動追随するが、ResizeClient() は表示スケールを考慮してくれない
+		Double scale = WinUi3Common.DisplayScale(_window);
+
+		Int32 width = _window.AppWindow.ClientSize.Width;
+		if (_window.SizeToContent == SizeToContent.Width || _window.SizeToContent == SizeToContent.WidthAndHeight)
+		{
+			width = (Int32)Content.ActualSize.X;
+			if (Content is FrameworkElement frameworkElement)
+			{
+				width += (Int32)(frameworkElement.Margin.Left + frameworkElement.Margin.Right);
+			}
+			width = (Int32)(width * scale);
+		}
+		Int32 height = _window.AppWindow.ClientSize.Height;
+		if (_window.SizeToContent == SizeToContent.Height || _window.SizeToContent == SizeToContent.WidthAndHeight)
+		{
+			height = (Int32)Content.ActualSize.Y;
+			if (Content is FrameworkElement frameworkElement)
+			{
+				height += (Int32)(frameworkElement.Margin.Top + frameworkElement.Margin.Bottom);
+			}
+			height = (Int32)(height * scale);
+		}
+		_window.AppWindow.ResizeClient(new SizeInt32(width, height));
 	}
 }
