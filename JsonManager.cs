e248805450625fca5b23f96cf8955dbcedfcbf7c
shinta0806 <shinta.0806@gmail.com>
2025-02-08 00:20:42 +0900

    唄詠 Sobremesa Ver 0.69 時点

--- JsonManager.cs
+++ JsonManager.cs
@@ -18,11 +18,13 @@
 // (1.01) | 2023/09/17 (Sun) |   Load() のオーバーロードを作成。
 // (1.04) | 2024/04/08 (Mon) |   MSIX ではない馬合の保存先を変更。
 // (1.05) | 2024/05/20 (Mon) |   Load() のパスを改善。
+//  1.10  | 2025/02/08 (Sat) | AOT 対応。
 // ============================================================================
 
 using System.IO.Compression;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
 
 namespace Shinta;
 
@@ -33,8 +35,9 @@ internal class JsonManager
 	// ====================================================================
 
 #pragma warning disable CA1822
+#if !USE_AOT
 	/// <summary>
-	/// 設定を読み込み
+	/// 設定を読み込み（AOT 非対応）
 	/// </summary>
 	/// <typeparam name="T"></typeparam>
 	/// <param name="path">相対パスの場合はデフォルトのフォルダー配下から読み込み</param>
@@ -49,7 +52,7 @@ internal class JsonManager
 	}
 
 	/// <summary>
-	/// 設定を読み込み
+	/// 設定を読み込み（AOT 非対応）
 	/// </summary>
 	/// <param name="type"></param>
 	/// <param name="path"></param>
@@ -62,9 +65,27 @@ internal class JsonManager
 		String json = LoadJsonString(path, decompress);
 		return JsonSerializer.Deserialize(json, type, options) ?? throw new Exception("設定を復元できませんでした：" + path);
 	}
+#endif
 
 	/// <summary>
-	/// 設定を保存
+	/// 設定を読み込み（AOT 対応）
+	/// AOT 非対応版で保存したデータを AOT 対応版で読み込むことは可能
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="path"></param>
+	/// <param name="decompress"></param>
+	/// <param name="jsonTypeInfo"></param>
+	/// <returns></returns>
+	/// <exception cref="Exception"></exception>
+	public T LoadAot<T>(String path, Boolean decompress, JsonTypeInfo<T> jsonTypeInfo)
+	{
+		String json = LoadJsonString(path, decompress);
+		return JsonSerializer.Deserialize(json, jsonTypeInfo) ?? throw new Exception("設定を復元できませんでした：" + path);
+	}
+
+#if !USE_AOT
+	/// <summary>
+	/// 設定を保存（AOT 非対応）
 	/// </summary>
 	/// <param name="settings"></param>
 	/// <param name="path">相対パスの場合はデフォルトのフォルダー配下に保存</param>
@@ -74,16 +95,22 @@ internal class JsonManager
 	public void Save(Object settings, String path, Boolean compress, JsonSerializerOptions? options = default)
 	{
 		String json = JsonSerializer.Serialize(settings, options);
-		path = AdjustPath(path);
-		Directory.CreateDirectory(Path.GetDirectoryName(path) ?? throw new Exception("設定保存先が不正です：" + path));
-		if (compress)
-		{
-			Compress(json, path);
-		}
-		else
-		{
-			File.WriteAllText(path, json, Encoding.UTF8);
-		}
+		SaveJsonString(json, path, compress);
+	}
+#endif
+
+	/// <summary>
+	/// 設定を保存（AOT 対応）
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="settings"></param>
+	/// <param name="path"></param>
+	/// <param name="decompress"></param>
+	/// <param name="jsonTypeInfo"></param>
+	public void SaveAot<T>(Object settings, String path, Boolean compress, JsonTypeInfo<T> jsonTypeInfo)
+	{
+		String json = JsonSerializer.Serialize(settings, jsonTypeInfo);
+		SaveJsonString(json, path, compress);
 	}
 #pragma warning restore CA1822
 
@@ -167,6 +194,26 @@ internal class JsonManager
 		else
 		{
 			return File.ReadAllText(path, Encoding.UTF8);
+		}
+	}
+
+	/// <summary>
+	/// JSON 文字列の保存
+	/// </summary>
+	/// <param name="json"></param>
+	/// <param name="path"></param>
+	/// <param name="compress"></param>
+	private static void SaveJsonString(String json, String path, Boolean compress)
+	{
+		path = AdjustPath(path);
+		Directory.CreateDirectory(Path.GetDirectoryName(path) ?? throw new Exception("設定保存先が不正です：" + path));
+		if (compress)
+		{
+			Compress(json, path);
+		}
+		else
+		{
+			File.WriteAllText(path, json, Encoding.UTF8);
 		}
 	}
 }
